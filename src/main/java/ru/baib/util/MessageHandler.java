a5e53c5b6a2f9d86413b99852f9a0cdebce1fc54
olliestyle <olliestyle1989@gmail.com>
2022-08-21 21:08:11 +0300

    Replace switch with map

--- src/main/java/ru/baib/util/MessageHandler.java
+++ src/main/java/ru/baib/util/MessageHandler.java
@@ -4,34 +4,28 @@ import ru.baib.model.ClientMessage;
 import ru.baib.model.ServerResponse;
 import ru.baib.service.ResponseService;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 public class MessageHandler {
 
     private final ResponseService responseService;
+    private final Map<String, Function<ClientMessage, ServerResponse>> functionMap = new HashMap<>();
 
     public MessageHandler(ResponseService responseService) {
         this.responseService = responseService;
+        functionMap.put("ru.baib.action.CreateAction", responseService::createTask);
+        functionMap.put("ru.baib.action.ShowAllAction", responseService::showAllTasks);
+        functionMap.put("ru.baib.action.LoginAction", responseService::login);
+        functionMap.put("ru.baib.action.RegisterAction", responseService::register);
     }
 
     public ServerResponse handleMessage(ClientMessage clientMessage) {
-        ServerResponse response;
+        ServerResponse response = new ServerResponse();
+        response.setStatus("Undefined action");
         String action = clientMessage.getAction();
-        switch (action) {
-            case "ru.baib.action.CreateAction":
-                response = responseService.createTask(clientMessage);
-            break;
-            case "ru.baib.action.ShowAllAction":
-                response = responseService.showAllTasks(clientMessage);
-            break;
-            case "ru.baib.action.LoginAction":
-                response = responseService.login(clientMessage);
-            break;
-            case "ru.baib.action.RegisterAction":
-                response = responseService.register(clientMessage);
-            break;
-            default:
-                response = new ServerResponse();
-                response.setStatus("Undefined action");
-        }
+        response = functionMap.get(action).apply(clientMessage);
         return response;
     }
 
